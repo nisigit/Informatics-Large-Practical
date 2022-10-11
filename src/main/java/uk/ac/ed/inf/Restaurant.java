50ffefe881b18a9dc9fb3a7ed8a0b27889fd032a
Nisiman <nisimanbhardwaj29@gmail.com>
2022-10-11 16:43:21 +0100

    getRestaurantFromRestServer implemented

--- src/main/java/uk/ac/ed/inf/Restaurant.java
+++ src/main/java/uk/ac/ed/inf/Restaurant.java
@@ -1,26 +1,44 @@
 package uk.ac.ed.inf;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class Restaurant {
 
     @JsonProperty("name")
-    private String name;
+    public String name;
 
     @JsonProperty("longitude")
-    private double lng;
+    public double lng;
 
     @JsonProperty("latitude")
-    private double lat;
+    public double lat;
 
     @JsonProperty("menu")
-    private Menu[] menu;
+    public Menu[] menuItems;
 
     public Restaurant() {
 
     }
 
     public Menu[] getMenu() {
+        return menuItems;
+    }
+
+    public static Restaurant[] getRestaurantsFromRestServer(URL serverBaseAddress) throws IOException {
+        String endPoint = "";
+
+        if (!serverBaseAddress.toString().endsWith("/")) {
+            endPoint = "/" + endPoint;
+        }
+        endPoint += "restaurants";
+
+        URL serverAddress = new URL(serverBaseAddress + endPoint);
+        return new ObjectMapper().readValue(
+                serverAddress, Restaurant[].class);
     }
 
 }
